41938429df6dcd55484771ce74e6b2c440bed557
jsmoya <jsmoyam@gmail.com>
2013-09-28 14:53:54 +0200

    Practica 2

--- progfun-funsets/src/test/scala/funsets/FunSetSuite.scala
+++ progfun-funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,12 +101,25 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+  
+  test("forall method") {
+    new TestSets {
+      val x = union(s1, s2)
+      assert(forall(x, s => s < 3), "testing with int 1")
+    }
+  }
+  
+  test("map method") {
+    new TestSets {
+
     }
   }
 }
